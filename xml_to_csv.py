c960ed141ee76d461b0e72d52b013f66b66a32d2
imamdigmi <imam.digmi@gmail.com>
2018-01-24 17:30:30 +0700

    Change dir name

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -27,7 +27,7 @@ def xml_to_csv(path):
 
 def main():
     for directory in ['train', 'test']:
-        image_path = os.path.join(os.getcwd(), 'images/{}'.format(directory))
+        image_path = os.path.join(os.getcwd(), 'annotations/{}'.format(directory))
         xml_df = xml_to_csv(image_path)
         xml_df.to_csv('data/{}_labels.csv'.format(directory), index=None)
         print('Successfully converted xml to csv.')
